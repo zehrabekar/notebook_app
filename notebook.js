5ac217001afeb1fdd6c094d33ec1ae527872b21b
Zehra Bekar <zehrabekar54@gmail.com>
2024-03-14 15:21:46 +0300

    Added local storage functionality for storing tasks

--- notebook.js
+++ notebook.js
@@ -6,12 +6,71 @@ const yeniGorevEkleBtn = document.querySelector(".btn-ekle");
 const gorevListesi = document.querySelector(".gorev-listesi");
 
 yeniGorevEkleBtn.addEventListener("click", gorevEkle);
+gorevListesi.addEventListener("click",gorevSilTamamla);
+document.addEventListener("DOMContentLoaded",localStorageOku);
 
 function gorevEkle(e){
     e.preventDefault();
+    
+    if(yeniGorev.value.trim() === "") { // Yeni eklenen görevin boş olup olmadığını kontrol ediyoruz
+        alert("Boş not kaydedilemiyor.");
+        return; // Boş not eklenirse işlemi sonlandırıyoruz
+    }
 
-    // input alanına girilen değeri görev listesi kısmına eklemek için html sayfasındaki yapıyı burada oluşturmalıyız
+    gorevItemOlustur(yeniGorev.value);
+    //localstoragea kaydet
+    localStorageKaydet(yeniGorev.value);
+    yeniGorev.value = "";
+};
 
+function localStorageArrayDonustur(){
+    let gorevler;
+
+    if(localStorage.getItem("gorevler")=== null){
+        gorevler = [];
+    }else{
+        gorevler= JSON.parse(localStorage.getItem("gorevler"))
+    }
+
+    return gorevler;
+};
+
+function gorevSilTamamla(e){
+    const tiklanilanEleman = e.target; // tıklanma olayının gerçekleştiği yeri verir
+
+    if(tiklanilanEleman.classList.contains("btn-tamamlanan-gorev")){
+        tiklanilanEleman.parentElement.classList.toggle("tamamlanan-gorev")
+    }
+    // burada tıklanılan elemanın parent elemanına gidilir ve tamamlanan-gorev sınıfı yoksa eklenir varsa silinir (toggle)
+
+    if(tiklanilanEleman.classList.contains("btn-gorev-sil")){
+       if(confirm("silmek istediğinize emin misiniz?")){
+        tiklanilanEleman.parentElement.classList.toggle("kaybol")
+        const silinecekGorev = tiklanilanEleman.parentElement.children[0].innerText;
+        localStorageSil(silinecekGorev);
+        tiklanilanEleman.parentElement.addEventListener("transitionend",function(){
+            tiklanilanEleman.parentElement.remove();
+        });
+    }
+       }
+};
+
+function localStorageKaydet(yeniGorev){
+    let gorevler = localStorageArrayDonustur();
+
+    gorevler.push(yeniGorev);
+    localStorage.setItem("gorevler", JSON.stringify(gorevler));
+};
+
+function localStorageOku(){
+    let gorevler = localStorageArrayDonustur();
+
+    gorevler.forEach(function(gorev){
+        gorevItemOlustur(gorev);
+    });
+}
+
+function gorevItemOlustur(gorev){
     //div oluşturma
     const gorevDiv = document.createElement("div");
     gorevDiv.classList.add("gorev-items");
@@ -19,7 +78,7 @@ function gorevEkle(e){
     //li oluşturma
     const gorevLi = document.createElement("li");
     gorevLi.classList.add("gorev-tanim");
-    gorevLi.innerText= yeniGorev.value;
+    gorevLi.innerText= gorev;
     gorevDiv.appendChild(gorevLi);
 
     //ul'ye oluşturduğumuz div'i ekleme
@@ -39,24 +98,15 @@ function gorevEkle(e){
     gorevSilBtn.innerHTML= '<i class="fa-regular fa-trash-can"></i>';
     gorevDiv.appendChild(gorevSilBtn);
 
-    // input alanına yazdığımız notu not listesine ekledikten sonra input alanını temizler :
-    yeniGorev.value = "";
 };
 
-gorevListesi.addEventListener("click",gorevSilTamamla);
-    
-function gorevSilTamamla(e){
-    const tiklanilanEleman = e.target; // tıklanma olayının gerçekleştiği yeri verir
+function localStorageSil(gorev){
+    let gorevler = localStorageArrayDonustur();
 
-    if(tiklanilanEleman.classList.contains("btn-tamamlanan-gorev")){
-        tiklanilanEleman.parentElement.classList.toggle("tamamlanan-gorev")
-    }
-    // burada tıklanılan elemanın parent elemanına gidilir ve tamamlanan-gorev sınıfı yoksa eklenir varsa silinir (toggle)
+    //splice ile item sil
+    const silinecekElemanIndex = gorevler.indexOf(gorev);
+    console.log(silinecekElemanIndex);
+    gorevler.splice(silinecekElemanIndex,1);
 
-    if(tiklanilanEleman.classList.contains("btn-gorev-sil")){
-        tiklanilanEleman.parentElement.classList.toggle("kaybol")
-        tiklanilanEleman.parentElement.addEventListener("transitionend",function(){
-            tiklanilanEleman.parentElement.remove();
-        });
-    }
+    localStorage.setItem("gorevler",JSON.stringify(gorevler));
 };
